0890a452f8c919565303a75d1e6dd860360fa0ef
Not_Shultz <59833741+MichaelCSturm@users.noreply.github.com>
2024-10-23 17:26:35 -0500

    Found way to access hidden nested json data
    It's workin alright

--- main.py
+++ main.py
@@ -4,4 +4,14 @@ import api
 import json
 
 data = api.getAndPrint()
-print(type(data))
+jsonData =json.dumps(data)
+#print(type(jsonData))
+#print
+#print(len(data))
+#print(jsonData)
+print(data['Meta Data'])
+for i in data['Meta Data']:
+    print(i)
+#     print(i)
+#     # for metadata in i:
+#     #     print(metadata)
